3d70060755e74f3ea2304dbe30f60ef67a5c4634
emTaiUS <huynhtrinhchitai0910@gmail.com>
2026-05-28 23:31:28 +0700

    Da toi uu code cu gan nhu la sua

--- Bai_B/Lan_2/code.cpp
+++ Bai_B/Lan_2/code.cpp
@@ -5,103 +5,164 @@
 using namespace std;
 
 const int MAXN = 100005;
-const int MAX_BUF = 15000000;
+const int MAX_BUF = 15000005;
+
 char in_buf[MAX_BUF];
 char out_buf[MAX_BUF];
+
 int out_ptr = 0;
 
 char* strings[MAXN];
 int str_len[MAXN];
-int idx[MAXN], tidx[MAXN];
-int bucket_map[256];
+
+int idx1[MAXN];
+int idx2[MAXN];
+
+inline void flush_out() {
+    fwrite(out_buf, 1, out_ptr, stdout);
+    out_ptr = 0;
+}
 
 inline void fast_write(const char* s, int len) {
-    memcpy(&out_buf[out_ptr], s, len);
+    memcpy(out_buf + out_ptr, s, len);
     out_ptr += len;
     out_buf[out_ptr++] = '\n';
+
     if (out_ptr > MAX_BUF - 200) {
-        fwrite(out_buf, 1, out_ptr, stdout);
-        out_ptr = 0;
+        flush_out();
     }
 }
 
-void msdRadix(int* id, int* td, int n, int depth) {
+inline int bucket(unsigned char c) {
+    return c ? (c - 'a' + 1) : 0;
+}
+
+inline bool less_suffix(const char* a, const char* b, int depth) {
+    a += depth;
+    b += depth;
+
+    while (*a == *b && *a) {
+        ++a;
+        ++b;
+    }
+
+    return *a < *b;
+}
+
+void insertion_sort(int* id, int n, int depth) {
+    for (int i = 1; i < n; ++i) {
+        int key = id[i];
+        int j = i - 1;
+
+        while (j >= 0 &&
+               less_suffix(strings[key], strings[id[j]], depth)) {
+            id[j + 1] = id[j];
+            --j;
+        }
+
+        id[j + 1] = key;
+    }
+}
+
+void msdRadix(int* src, int* dst, int n, int depth) {
     if (n <= 1) return;
 
     if (n <= 32) {
-        for (int i = 1; i < n; ++i) {
-            int key = id[i];
-            int j = i - 1;
-            while (j >= 0 && strcmp(strings[id[j]] + depth, strings[key] + depth) > 0) {
-                id[j + 1] = id[j];
-                --j;
-            }
-            id[j + 1] = key;
-        }
+        insertion_sort(src, n, depth);
         return;
     }
 
-    int count[28] = {0};
+    int count[27] = {};
+    int start[27];
+    int pos[27];
+
     for (int i = 0; i < n; ++i) {
-        count[bucket_map[(unsigned char)strings[id[i]][depth]]]++;
+        ++count[bucket((unsigned char)strings[src[i]][depth])];
     }
 
-    int start[28];
     start[0] = 0;
-    for (int i = 1; i <= 27; ++i) start[i] = start[i - 1] + count[i - 1];
-
-    int pos[28];
-    memcpy(pos, start, sizeof(start));
-    for (int i = 0; i < n; ++i) {
-        int item = id[i];
-        td[pos[bucket_map[(unsigned char)strings[item][depth]]]++] = item;
+    for (int i = 1; i < 27; ++i) {
+        start[i] = start[i - 1] + count[i - 1];
     }
 
-    memcpy(id, td, n * sizeof(int));
+    memcpy(pos, start, sizeof(start));
 
-    for (int b = 1; b <= 26; ++b) {
-        if (count[b] > 1) {
-            msdRadix(id + start[b], td + start[b], count[b], depth + 1);
+    for (int i = 0; i < n; ++i) {
+        int v = src[i];
+        dst[pos[bucket((unsigned char)strings[v][depth])]++] = v;
+    }
+
+    memcpy(src, dst, n * sizeof(int));
+
+    for (int b = 1; b < 27; ++b) {
+        int sz = count[b];
+
+        if (sz > 1) {
+            msdRadix(src + start[b],
+                     dst + start[b],
+                     sz,
+                     depth + 1);
         }
     }
 }
 
-int main() {
-    size_t bytes = fread(in_buf, 1, MAX_BUF, stdin);
-    if (bytes <= 0) return 0;
-    in_buf[bytes] = '\0';
+inline void write_int(int x) {
+    char s[16];
+    int len = 0;
 
-    bucket_map[0] = 0;
-    for (int c = 'a'; c <= 'z'; ++c) {
-        bucket_map[c] = c - 'a' + 1;
+    do {
+        s[len++] = char('0' + x % 10);
+        x /= 10;
+    } while (x);
+
+    while (len--) {
+        out_buf[out_ptr++] = s[len];
     }
+
+    out_buf[out_ptr++] = '\n';
+}
+
+int main() {
+    size_t bytes = fread(in_buf, 1, MAX_BUF - 1, stdin);
+
+    if (!bytes) return 0;
+
+    in_buf[bytes] = '\0';
 
     int p = 0;
     int n = 0;
-    while (in_buf[p] < '0' || in_buf[p] > '9') p++;
-    while (in_buf[p] >= '0' && in_buf[p] <= '9') n = n * 10 + (in_buf[p++] - '0');
+
+    while (in_buf[p] < '0' || in_buf[p] > '9') ++p;
+
+    while (in_buf[p] >= '0' && in_buf[p] <= '9') {
+        n = n * 10 + (in_buf[p++] - '0');
+    }
 
     for (int i = 0; i < n; ++i) {
-        while (in_buf[p] != '\0' && (in_buf[p] < 'a' || in_buf[p] > 'z')) p++;
+        while (in_buf[p] < 'a' || in_buf[p] > 'z') ++p;
+
         strings[i] = &in_buf[p];
-        idx[i] = i;
-        int start_p = p;
-        while (in_buf[p] >= 'a' && in_buf[p] <= 'z') p++;
-        str_len[i] = p - start_p;
+        idx1[i] = i;
+
+        int st = p;
+
+        while (in_buf[p] >= 'a' && in_buf[p] <= 'z') ++p;
+
+        str_len[i] = p - st;
+
         in_buf[p++] = '\0';
     }
 
-    msdRadix(idx, tidx, n, 0);
+    msdRadix(idx1, idx2, n, 0);
 
-    out_ptr = sprintf(out_buf, "%d\n", n);
+    write_int(n);
+
     for (int i = 0; i < n; ++i) {
-        int gid = idx[i];
-        fast_write(strings[gid], str_len[gid]);
+        int id = idx1[i];
+        fast_write(strings[id], str_len[id]);
     }
 
-    if (out_ptr > 0) {
-        fwrite(out_buf, 1, out_ptr, stdout);
-    }
+    if (out_ptr) flush_out();
 
     return 0;
 }
